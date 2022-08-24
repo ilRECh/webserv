099a32dc5b42210b2f034e1cdcb6108ae1844a46
ilRECh <ildar.rch@gmail.com>
2022-08-25 00:02:46 +0200

    Working on parsing Location block

--- include/Config/LocationBlock.hpp
+++ include/Config/LocationBlock.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ABlock.hpp"
+#include <map>
 
 class ConfigFile;
 
@@ -9,6 +10,18 @@ class LocationBlock : public ABlock<LocationBlock>
 private:
     LocationBlock(LocationBlock &that);
     LocationBlock& operator=(LocationBlock &that);
+
+    std::set<std::string> Method_names;
+    std::string Root;
+    std::string Index;
+    bool Autoindex;
+    std::pair<int, std::string> Return; //Redirection parameter
+
+    void parse_method_names();
+    void parse_root();
+    void parse_index();
+    void parse_autoindex();
+    void parse_return();
 
 public:
     LocationBlock(ConfigFile & config);
