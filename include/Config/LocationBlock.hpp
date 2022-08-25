b2519615fa35d3d4e1a88a8885e912e84fb295b8
ilRECh <ildar.rch@gmail.com>
2022-08-25 12:51:38 +0200

    Working on parsing Location block

--- include/Config/LocationBlock.hpp
+++ include/Config/LocationBlock.hpp
@@ -12,17 +12,26 @@ private:
     LocationBlock(LocationBlock &that);
     LocationBlock& operator=(LocationBlock &that);
 
+    static const char * Possible_allow_methods_setup [];
+    std::vector<std::string> Possible_allow_methods;
+
     int Autoindex;
-    std::pair<int, std::string> Return; //Redirection parameter
+    std::string Return; //Redirection parameter
     std::set<std::string> Allow_methods;
     std::string Root;
     std::string Index;
+    std::string FastCGI_path;
+    std::string Upload_pass; //URL, where the uploading starts
+    std::string Upload_store; //Path, where to store uploaded files
 
-    void parse_allow_methods(std::string value);
+    void parse_allow_methods(std::string values);
     void parse_root(std::string value);
     void parse_index(std::string value);
     void parse_autoindex(std::string value);
     void parse_return(std::string value);
+    void parse_fastcgi_path(std::string value);
+    void parse_upload_pass(std::string value);
+    void parse_upload_store(std::string value);
 
 public:
     LocationBlock(ConfigFile & config);
--- srcs/config/LocationBlock.cpp
+++ srcs/config/LocationBlock.cpp
@@ -1,56 +1,194 @@
 #include "LocationBlock.hpp"
+#include <algorithm>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+const char * LocationBlock::Possible_allow_methods_setup[] =
+{
+    "GET"    ,
+    "POST"   ,
+    "DELETE"
+};
 
 /**
  * @brief location block
- * allow_methods - define a list of accepted HTTP methods for the route.
- * root          - define a directory or a file from where the file should be searched
- * index         - set a default file to answer if the request is a directory.
- * autoindex     - turn on or off directory listing.
- * return        - define a HTTP redirection.
- * fastcgi_index - execute CGI based on certain file extension.
- * upload_pass   - make the route able to accept uploaded files.
- * upload_store  - configure where uploaded files should be saved.
+ * allow_methods           - define a list of accepted HTTP methods for the route.
+ * root                    - define a directory or a file from where the file should be searched
+ * index                   - set a default file to answer if the request is a directory.
+ * autoindex               - turn on or off directory listing.
+ * return 301              - define a HTTP redirection.
+ * fastcgi_param PATH_INFO - execute CGI based on certain file extension. 
+ * upload_pass             - make the route able to accept uploaded files.
+ * upload_store            - configure where uploaded files should be saved.
  */
-// ABlock::ParamCallback ConfigFile::setup_params[] =
-// {
-//     { "allow_methods " ,    //method_name [method_name [method_name]]
-//     { "root "          ,    //path
-//     { "index "         ,    //path
-//     { "autoindex "     ,    //on/off
-//     { "return "             //code uri
-// };
-
 LocationBlock::LocationBlock(ConfigFile & config)
     :   ABlock(config),
-        Autoindex(-1),
-        Return(std::make_pair(-1, ""))
+        Possible_allow_methods(Possible_allow_methods_setup, Possible_allow_methods_setup + (sizeof(Possible_allow_methods_setup) / sizeof(const char *))),
+        Autoindex(-1)
 {
+    //method_name [method_name [method_name]]
+    parsers.push_back(
+        ABlock::ParamCallback("allow_methods ", std::mem_fun(&LocationBlock::parse_allow_methods), this)
+    );
+
+    //path
+    parsers.push_back(
+        ABlock::ParamCallback("root ", std::mem_fun(&LocationBlock::parse_root), this)
+    );
+
+    //path
+    parsers.push_back(
+        ABlock::ParamCallback("index ", std::mem_fun(&LocationBlock::parse_index), this)
+    );
+
+    //on/off
+    parsers.push_back(
+        ABlock::ParamCallback("autoindex ", std::mem_fun(&LocationBlock::parse_autoindex), this)
+    );
+
+    //code uri
+    parsers.push_back(
+        ABlock::ParamCallback("return 301 ", std::mem_fun(&LocationBlock::parse_return), this)
+    );
+
+    //code uri
+    parsers.push_back(
+        ABlock::ParamCallback("fastcgi_param PATH_INFO ", std::mem_fun(&LocationBlock::parse_fastcgi_path), this)
+    );
+
+    //code uri
+    parsers.push_back(
+        ABlock::ParamCallback("upload_pass ", std::mem_fun(&LocationBlock::parse_upload_pass), this)
+    );
+
+    //code uri
+    parsers.push_back(
+        ABlock::ParamCallback("upload_store ", std::mem_fun(&LocationBlock::parse_upload_store), this)
+    );
+
     OUT_DBG("Constructor");
 }
 
-void LocationBlock::parse_allow_methods(std::string value)
+void LocationBlock::parse_allow_methods(std::string values)
 {
-(void) value;
+    if (Allow_methods.empty())
+    {
+        char * values_buf = new char[values.length() + 1];
+
+        strcpy(values_buf, values.c_str());
+
+        char * method = std::strtok(values_buf, " ");
+
+        while (method != NULL)
+        {
+            if (std::find(Possible_allow_methods.begin(), Possible_allow_methods.end(), std::string(method))
+                != Possible_allow_methods.end())
+            {
+                
+                Allow_methods.insert(method);
+            }
+
+            method = std::strtok(NULL, " ");
+        }
+
+        delete values_buf;
+    }
 }
 
 void LocationBlock::parse_root(std::string value)
 {
-(void) value;
+    if (Root.empty())
+    {
+        struct stat sb;
+
+        if (stat(value.c_str(), &sb) == 0)
+        {
+            if (S_ISDIR(sb.st_mode))
+            {
+                Root = value;
+            }
+        }
+    }
 }
 
 void LocationBlock::parse_index(std::string value)
 {
-(void) value;
+    if (Index.empty())
+    {
+        struct stat sb;
+
+        if (stat(value.c_str(), &sb) == 0)
+        {
+            if (S_ISREG(sb.st_mode))
+            {
+                Index = value;
+            }
+        }
+    }
 }
 
 void LocationBlock::parse_autoindex(std::string value)
 {
-(void) value;
+    if (Autoindex == -1)
+    {
+        if (value == "on")
+        {
+            Autoindex = true;
+        }
+        else if (value == "off")
+        {
+            Autoindex = false;
+        }
+    }
 }
 
 void LocationBlock::parse_return(std::string value)
 {
-(void) value;
+    if (Return.empty())
+    {
+        Return = value;
+    }
+}
+
+void LocationBlock::parse_fastcgi_path(std::string value)
+{
+    if (FastCGI_path.empty())
+    {
+        struct stat sb;
+
+        if (stat(value.c_str(), &sb) == 0)
+        {
+            if (S_ISREG(sb.st_mode))
+            {
+                FastCGI_path = value;
+            }
+        }
+    }
+}
+
+void LocationBlock::parse_upload_pass(std::string value)
+{
+    if (Upload_pass.empty())
+    {
+        Upload_pass = value;
+    }
+}
+
+void LocationBlock::parse_upload_store(std::string value)
+{
+    if (Upload_store.empty())
+    {
+        struct stat sb;
+
+        if (stat(value.c_str(), &sb) == 0)
+        {
+            if (S_ISDIR(sb.st_mode))
+            {
+                Upload_store = value;
+            }
+        }
+    }
 }
 
 void LocationBlock::validate()
@@ -59,7 +197,7 @@ void LocationBlock::validate()
         Root.empty()          or
         Index.empty()         or
         Autoindex == -1       or
-        Return.first == -1)
+        Return.empty())
     {
         throw ERR("Invalid location block");
     }
